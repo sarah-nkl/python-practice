0cdca10b69c037084f89aad8ff4d4c6280604721
Sarah Neo <sarah.neo@motional.com>
2023-08-13 19:01:30 +0800

    new_path_right now goes to the right instead of left

--- number_of_ways.py
+++ number_of_ways.py
@@ -24,14 +24,14 @@ def numberOfWays(startPos: int, endPos: int, k: int) -> int:
             new_path = path.copy()
             last_position = new_path[-1]
 
-            # exist fast if not going to make to end
+            # exit fast if not going to make to end
             if endPos - last_position > (k - i - 1):
                 continue
             # path that goes to the left
             new_path_left = new_path + [last_position - 1]
 
             # path that goes to the right
-            new_path_right = new_path + [last_position - 1]
+            new_path_right = new_path + [last_position + 1]
 
             # add paths to the left and right
             paths.append(new_path_left)
